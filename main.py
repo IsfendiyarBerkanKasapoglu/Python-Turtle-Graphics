100b40bab66085c30aa26df1bcd781995379f8b3
IsfendiyarBerkanKasapoglu <berkankasapoglu03@hotmail.com>
2022-08-25 16:38:38 +0300

    Turtle graphics

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+t = turtle.Turtle()
+t.speed(0)
+
+sides = 3;
+colors = ['red', 'yellow', 'orange']
+
+for x in range(360):
+    t.pencolor(colors[x % sides])
+    t.forward(x * 3 / sides + x)
+    t.left(360 / sides + 1)
+    t.width(x * sides / 200)
+
